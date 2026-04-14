f02a847842024da30ee05f1cd595d6c4734224d8
Helpmegg <omdenis2005@gmail.com>
2026-04-14 12:23:56 +0200

    Resolve

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -26,6 +26,10 @@ resource "google_container_cluster" "primary" {
       cidr_block   = "10.0.0.0/8"
       display_name = "VPC CIDR"
     }
+    cidr_blocks {
+      cidr_block   = "0.0.0.0/0"
+      display_name = "GitHub Actions"
+    }
   }
 
 
